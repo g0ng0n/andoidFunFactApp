a10e9abb89754ac6b6f3d5bbc229141667160596
g0ng0n <gonzalo.a.gisbert@gmail.com>
2017-05-06 17:11:54 -0300

    commit

---
+++ app/src/main/java/com/example/gonza/funfacts/ColorWheel.java
@@ -0,0 +1,34 @@
+package com.example.gonza.funfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+public class ColorWheel {
+    // Fields
+    private String[] mColors = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    // Methods - Actions the object can take
+    public int getColor(){
+        // Ramdonly select a fact
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(mColors.length);
+        String color = mColors[randomNumber];
+        int colorAsInt = Color.parseColor(color);
+        return colorAsInt;
+    }
+}
